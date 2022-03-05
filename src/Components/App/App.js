45f099662cff5f878fdff40d33fdb17926b9a1aa
TheGoldenDev <Spencer.uab@gmail.com>
2022-03-05 03:28:40 -0500

    disable auto redirect

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -65,7 +65,7 @@ class App extends React.Component {
 
   componentDidMount() {
     window.addEventListener('load', () => {
-      Spotify.getAccessToken();
+      //Spotify.getAccessToken();
     });
   }
 
